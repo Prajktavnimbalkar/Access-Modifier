d3f016d4cfc135b55eddaf43ec50a8e1c9aac360
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-02-26 09:07:54 +0530

    Add files via upload

---
+++ Acceedemo.java
@@ -0,0 +1,11 @@
+package acessmodifier;
+
+public class Acceedemo {
+private int a =18;
+public void showdemo() {
+	System.out.println("value = "+ a);
+	}
+private void testdemo () {
+	System.out.println("not acess in anathoer class");
+}
+}
