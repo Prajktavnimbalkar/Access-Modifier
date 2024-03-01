8e3b265cbe4a73d62de4aab08f6ba097254836c7
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2024-03-01 09:13:23 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,11 @@
+package acessmodifier;
+
+public class sample {
+	public static void main(String[] args) {
+		aa A = new aa();
+		System.out.println(A.data1);
+		System.out.println(A.data);
+		A.msg();
+	}
+
+}
